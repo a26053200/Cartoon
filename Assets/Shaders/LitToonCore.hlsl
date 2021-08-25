f877985590699330a6beb639a2037727b30e0b18
a26053200 <zhengnan262@qq.com>
2021-08-26 00:42:11 +0800

    New Models

---
+++ Assets/Shaders/LitToonCore.hlsl
@@ -0,0 +1,26 @@
+﻿
+
+Varyings vert(Attributes v)
+{
+    Varyings o;
+    
+    VertexPositionInputs positionInputs = GetVertexPositionInputs(v.positionOS.xyz);
+    o.positionCS = positionInputs.positionCS;
+    o.uv = TRANSFORM_TEX(v.uv, _BaseMap);
+    
+    VertexNormalInputs vertexNormalInput = GetVertexNormalInputs(v.normal.xyz);
+    o.normal = vertexNormalInput.normalWS;
+    
+    o.color = v.color;
+    return o;
+}
+
+
+half4 frag(Varyings i): SV_Target
+{
+    half4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, i.uv);
+    
+    float3 final = baseMap.rgb;
+    
+    return float4(final, 1);
+}
---
+++ Assets/Shaders/LitToonProperty.hlsl
@@ -0,0 +1,25 @@
+﻿struct Attributes
+{
+    float4 positionOS: POSITION;
+    float2 uv: TEXCOORD0;
+    float4 normal: NORMAL;
+    float3 color: COLOR;
+};
+
+struct Varyings
+{
+    float4 positionCS: SV_POSITION;
+    float2 uv: TEXCOORD0;
+    float3 positionWS: TEXCOORD1;
+    float3 normal: TEXCOORD2;
+    float3 color: TEXCOORD6;
+};
+
+TEXTURE2D(_BaseMap); SAMPLER(sampler_BaseMap);
+TEXTURE2D(_LightMap); SAMPLER(sampler_LightMap);
+TEXTURE2D(_RampMap); SAMPLER(sampler_RampMap);
+    
+CBUFFER_START(UnityPerMaterial)
+float4 _BaseMap_ST, _LightMap_ST, _RampMap_ST;
+float4 _BaseColor;
+CBUFFER_END
